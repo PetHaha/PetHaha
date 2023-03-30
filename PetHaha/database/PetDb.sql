5ee7e3948e730ab642f4657a9cbbbe1e6d51ffb2
jinnspace <blooomingj@gmail.com>
2023-03-30 18:10:52 +0900

    add/ 정보수정 기능 구현

--- PetHaha/database/PetDb.sql
+++ PetHaha/database/PetDb.sql
@@ -1,7 +1,7 @@
-insert into PMember(id, pwd, nick, phone, name, intro, MemImg)
-values('yijin','1234','이진이진','010-1234-5678','채이진', '강아지 조아용', '');
-insert into PMember(id, pwd, nick, phone, name, intro, MemImg)
-values('daeui','1234','대의대의','010-5678-1234','김대의', '고양이 조아용', '');
+insert into PMember(id, pwd, nick, phone, name, intro)
+values('yijin','1234','이진이진','010-1234-5678','채이진', '강아지 조아용');
+insert into PMember(id, pwd, nick, phone, name, intro)
+values('daeui','1234','대의대의','010-5678-1234','김대의', '고양이 조아용');
 
 
 insert into PBanner(bnseq,bnborder,bnimage,bnname)
--- PetHaha/database/이진.sql
+++ PetHaha/database/이진.sql
@@ -4,3 +4,21 @@ BEGIN
     OPEN p_curvar For 
     select * from pmember where id=p_id;
 END;
+
+create or replace procedure PmemberUpdate(
+    p_id in varchar2,
+    p_pwd in varchar2,
+    p_nick in varchar2,
+    p_phone in varchar2,
+    p_name in varchar2,
+    p_intro in varchar2,
+    p_memimg in varchar2,
+    p_curvar out sys_refcursor
+)
+is
+begin
+    update pmember set pwd=p_pwd,nick=p_nick,phone=p_phone,name=p_name,intro=p_intro,memimg=p_memimg where id=p_id;
+    commit;
+     OPEN p_curvar For select * from member where id=p_id;
+end;
+
