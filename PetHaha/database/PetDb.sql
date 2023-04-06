1ee627abc354309c5fd24459b6f0a7eef4260402
jinnspace <blooomingj@gmail.com>
2023-04-06 20:46:59 +0900

    add/ msg 임시 데이너 넣기

--- PetHaha/database/PetDb.sql
+++ PetHaha/database/PetDb.sql
@@ -9,3 +9,12 @@ values(pbanner_seq.nextVal, 1, 'pet1.jpg', 'pet1');
 
 insert into PBanner(bnseq,bnborder,bnimage,bnname)
 values(pbanner_seq.nextVal, 2, 'pet2.jpg', 'pet2');
+
+insert into pmessage(msnum,tonick,id,nick,mcontent,answer,mcheck)
+values(pmember_seq.nextVal,'대의대의','yijin','이진이진','안녕하세요 반갑습니다','네 반가워요','0');
+
+insert into pmessage(msnum,tonick,id,nick,mcontent,answer,mcheck)
+values(pmessage_seq.nextVal,'대의대의','yijin','이진이진','안녕하세요 반갑습니다','네 반가워요','0');
+
+insert into pmessage(msnum,tonick,id,nick,mcontent,answer,mcheck)
+values(pmessage_seq.nextVal,'이진이진','daeui','대의대의','안녕하세요 반갑습니다','누구시죠?','1');
