83977d8e5d4b14eb54372a071f44b5a741b002e2
jinnspace <blooomingj@gmail.com>
2023-04-06 21:06:04 +0900

    modify/ db 수정

--- PetHaha/database/PetDb.sql
+++ PetHaha/database/PetDb.sql
@@ -13,8 +13,8 @@ values(pbanner_seq.nextVal, 2, 'pet2.jpg', 'pet2');
 insert into pmessage(msnum,tonick,id,nick,mcontent,answer,mcheck)
 values(pmember_seq.nextVal,'대의대의','yijin','이진이진','안녕하세요 반갑습니다','네 반가워요','0');
 
-insert into pmessage(msnum,tonick,id,nick,mcontent,answer,mcheck)
-values(pmessage_seq.nextVal,'대의대의','yijin','이진이진','안녕하세요 반갑습니다','네 반가워요','0');
+insert into pmessage(msnum,tonick,id,nick,mcontent,answer,mcheck,mtitle)
+values(pmessage_seq.nextVal,'대의대의','yijin','이진이진','안녕하세요 안녕하세요 반갑습니다 안녕하세요 반갑습니다','네 반가워요','0','안녕하세요 반갑습니다');
 
-insert into pmessage(msnum,tonick,id,nick,mcontent,answer,mcheck)
-values(pmessage_seq.nextVal,'이진이진','daeui','대의대의','안녕하세요 반갑습니다','누구시죠?','1');
+insert into pmessage(msnum,tonick,id,nick,mcontent,answer,mcheck,mtitle)
+values(pmessage_seq.nextVal,'이진이진','daeui','대의대의','저 기억하시죠???','누구시죠?','1','안녕하세요 반갑습니다');
