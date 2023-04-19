028a1efe7d15084231df92b3c84cde1509c3b5c6
76chel <dae9840@gmail.com>
2023-04-19 23:06:08 +0900

    add/ reportVO

---
+++ PetHaha/src/main/java/pethaha/dto/ReportVO.java
@@ -0,0 +1,14 @@
+package pethaha.dto;
+
+import lombok.Data;
+
+@Data
+public class ReportVO {
+
+	private int REPORTNUM;
+	private int RCATEGORY;
+	private int BNUM;
+	private String RCONTENT;
+	private String ID;
+	private String NICK;
+}
